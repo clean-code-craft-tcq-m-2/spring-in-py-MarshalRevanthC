7beb20b1d664bc88d26ad555567385d682591f12
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-10-13 21:38:07 +0530

    Update statistics.py
    declaration of float nan for empty array

--- statistics.py
+++ statistics.py
@@ -1,8 +1,9 @@
 def calculateStats(numbers):
   if len(numbers) == 0 :
-      return float("None")
+      x = float('nan')
+      return x
   else:
       avg = sum(numbers)/len(numbers)
       max = max(numbers)
       min = min(numbers)
-      return avg, max, min
+      return avg , max , min
