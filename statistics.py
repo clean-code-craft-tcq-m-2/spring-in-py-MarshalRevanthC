f80bd070977ae3a6895d8e9b70fbc36a0c967bb6
Marshal Revanth C <cr.marshalrevanth@gmail.com>
2022-10-13 21:14:22 +0530

    Update statistics.py
    none zero update

--- statistics.py
+++ statistics.py
@@ -1,6 +1,8 @@
-
 def calculateStats(numbers):
-    avg = sum(numbers) / len(numbers)
-    max = max(numbers)
-    min = min(numbers)
-    return avg, max, min
+  if len(numbers) == 0 :
+      return float("None")
+  else:
+      avg = sum(numbers)/len(numbers)
+      max = max(numbers)
+      min = min(numbers)
+      return avg, max, min
